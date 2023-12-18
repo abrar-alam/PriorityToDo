2a32dde678fd1e4b19b55602802a8358dfdc4c03
Abrar Alam <abraralam246@gmail.com>
2023-12-17 19:51:42 -0500

    add task add functionality prototype

--- src/app.js
+++ src/app.js
@@ -4,6 +4,17 @@ const to_do_section = document.querySelector("section.to_dos");
 // let element_to_create = null;
 // let 
 
+function return_priority(){
+    if (document.getElementById('essential').checked) {
+        return '&#128681;';
+      }
+    else if (document.getElementById('important').checked){
+        return '&#128314;';
+    }
+    return '&#127793;';
+};
+
+
 add_btn.addEventListener("click", function(e){
     e.preventDefault();
     console.log(form_data.querySelector("#task_name_field").value);
@@ -13,7 +24,29 @@ add_btn.addEventListener("click", function(e){
     li_to_create.innerText = form_data.querySelector("#task_name_field").value;
     date_span.innerText = form_data.querySelector("#completionDate").value;
     date_span.className = "list_dates";
+    priority_span.innerHTML = return_priority();
+
+
     li_to_create.append(date_span);
+    li_to_create.append(priority_span);
     to_do_section.querySelector("#tasks_to_do_list").append(li_to_create);
+
+    let entry = {'section': 'to_dos', 'task_name':form_data.querySelector("#task_name_field").value,
+                    'due_date':date_span.innerText, 'done': 'no',
+                    'removed':'no', 'priority': priority_span.innerText};
+
+    // set the local storage logic
+    if (! localStorage.getItem("user_data")){
+        
+
+        console.log(entry);
+
+        localStorage.setItem("user_data", JSON.stringify([entry]));
+    }
+    else {
+        let user_data_arr = JSON.parse(localStorage.getItem("user_data"));
+        user_data_arr.unshift(entry);
+        localStorage.setItem("user_data", JSON.stringify(user_data_arr));
+    }
     
 });
