f25c43f33dd2f346e29f64f06f0f6bb52a5e08e3
Abrar Alam <abraralam246@gmail.com>
2023-12-18 21:21:29 -0500

    add the fully functioning prototype

--- src/app.js
+++ src/app.js
@@ -2,14 +2,45 @@ const add_btn = document.querySelector("button#add_task");
 const form_data = document.querySelector("form#task-form");
 const to_do_section = document.querySelector("section.to_dos");
 const to_do_ul = to_do_section.querySelector("ul");
-// let element_to_create = null;
-// let 
 
-function return_priority(){
+
+
+if (localStorage.getItem("user_data")) {
+    let user_data_arr = JSON.parse(localStorage.getItem("user_data"));
+    for (let user of user_data_arr) {
+        let li_to_create = document.createElement("li");
+        let date_span = document.createElement("span");
+        let priority_span = document.createElement("span");
+        li_to_create.innerText = user.task_name;
+        date_span.innerText = user.due_date;
+        date_span.className = "list_dates";
+        priority_span.innerHTML = user.priority;
+        
+       
+
+        btn_remove = document.createElement("button");
+        btn_remove.className = 'removeButton';
+        btn_remove.innerText = 'remove';
+
+        li_to_create.append(date_span);
+        li_to_create.append(priority_span);
+
+        if (!(user.done === 'yes'))
+        {btn_finished = document.createElement("button");
+        btn_finished.className = 'finshedButton';
+        li_to_create.append(btn_finished);
+        btn_finished.innerText = 'finished';}
+        
+        li_to_create.append(btn_remove);
+        li_to_create.className = user.done === 'yes' ? 'done': 'task_to_do';
+        to_do_section.querySelector("#tasks_to_do_list").append(li_to_create);
+    }
+}
+function return_priority() {
     if (document.getElementById('essential').checked) {
         return '&#128681;';
-      }
-    else if (document.getElementById('important').checked){
+    }
+    else if (document.getElementById('important').checked) {
         return '&#128314;';
     }
     return '&#127793;';
@@ -17,7 +48,7 @@ function return_priority(){
 
 
 
-add_btn.addEventListener("click", function(e){
+add_btn.addEventListener("click", function (e) {
     e.preventDefault();
     console.log(form_data.querySelector("#task_name_field").value);
     let li_to_create = document.createElement("li");
@@ -39,15 +70,21 @@ add_btn.addEventListener("click", function(e){
     li_to_create.append(priority_span);
     li_to_create.append(btn_finished);
     li_to_create.append(btn_remove);
+    li_to_create.className = 'task_to_do';
     to_do_section.querySelector("#tasks_to_do_list").prepend(li_to_create);
+    
 
-    let entry = {'section': 'to_dos', 'task_name':form_data.querySelector("#task_name_field").value,
-                    'due_date':date_span.innerText, 'done': 'no',
-                    'removed':'no', 'priority': priority_span.innerText};
+    let entry = {
+        'section': 'to_dos', 'task_name': form_data.querySelector("#task_name_field").value,
+        'due_date': date_span.innerText, 'done': 'no',
+        'removed': 'no', 'priority': priority_span.innerText
+    };
+
+    form_data.reset();
 
     // set the local storage logic
-    if (! localStorage.getItem("user_data")){
-        
+    if (!localStorage.getItem("user_data")) {
+
 
         console.log(entry);
 
@@ -58,23 +95,32 @@ add_btn.addEventListener("click", function(e){
         user_data_arr.unshift(entry);
         localStorage.setItem("user_data", JSON.stringify(user_data_arr));
     }
-    
+
 });
 
-to_do_ul.addEventListener("click", function(e){
-    if (e.target.className ==='removeButton'){
+to_do_ul.addEventListener("click", function (e) {
+
+    let idx = Array.prototype.indexOf.call(e.target.parentElement.parentElement.children, e.target.parentElement);
+    let user_info_arr = JSON.parse(localStorage.getItem("user_data"));
+
+    if (e.target.className === 'removeButton') {
         console.dir(e.target);
         console.dir(Array.prototype.indexOf.call(e.target.parentElement.parentElement.children, e.target.parentElement));
-        
+
         // Now delete this task from the local storage
-        let idx = Array.prototype.indexOf.call(e.target.parentElement.parentElement.children, e.target.parentElement);
-        let user_info_arr = JSON.parse(localStorage.getItem("user_data"));
+
         user_info_arr.splice(idx, 1);
         // Now we can safely remove the task
         e.target.parentElement.remove();
-        //Update the local storage
-        localStorage.setItem("user_data", JSON.stringify(user_info_arr));
     }
 
-    //Continue from here
-})
+    else if (e.target.className === 'finshedButton') {
+        e.target.parentElement.className = 'done';
+        e.target.remove();
+        user_info_arr[idx].done = 'yes';
+    }
+    // Update the local storage
+    localStorage.setItem("user_data", JSON.stringify(user_info_arr));
+});
+
+
